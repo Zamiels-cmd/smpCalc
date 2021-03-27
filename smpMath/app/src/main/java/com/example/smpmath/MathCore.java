391c84d640db9c995f6ee0a9293843261f08ede2
David Angelo <54418832+empCarnivorePewPew@users.noreply.github.com>
2021-03-26 20:26:59 -0500

    safe simp calc

--- smpMath/app/src/main/java/com/example/smpmath/MathCore.java
+++ smpMath/app/src/main/java/com/example/smpmath/MathCore.java
@@ -3,61 +3,6 @@ package com.example.smpmath;
 import java.util.regex.Pattern;
 
 public class MathCore {
-    public class multNode{
-        Object left;
-        Object right;
-
-    }
-
-    public class addNode{
-        Object left;
-        Object right;
-    }
-
-    public class powNode{
-        Object left;
-        Object right;
-    }
-
-    public class groupNode{
-        Object inner;
-    }
-
-    public class varNod{
-        varNod(){
-        }
-    }
-
-    public class number{
-        double real;
-        double imag;
-
-        public number(double real, double imag) {
-            this.real = real;
-            this.imag = imag;
-        }
-
-        public double getImag() {
-            return imag;
-        }
-
-        public double getReal() {
-            return real;
-        }
-
-        public void setReal(double real) {
-            this.real = real;
-        }
-
-        public void setImag(double imag) {
-            this.imag = imag;
-        }
-
-        @Override
-        public String toString() {
-            return " "+String.valueOf(real)+" + "+String.valueOf(imag)+" * i ";
-        }
-    }
 
     static public Boolean isInt(String value){
         Pattern pattern = Pattern.compile("\\d+");
